52e97eaf6352fccb278d261b00e09dfcb64f973f
Ketan <ketantoo@gmail.com>
2020-11-09 15:18:11 +0530

    adding stream to app

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,6 +1,8 @@
 import "dart:async";
 
-import "./models//timer_model.dart";
+import 'package:my_time_app_flutter/models/timer_model.dart';
+
+import "./models/timer_model.dart";
 
 class CountDownTimer {
   double _radius = 1;
@@ -8,4 +10,32 @@ class CountDownTimer {
   Timer timer;
   Duration _time;
   Duration _fullTime;
+  String returnTime(Duration t) {
+    String minutes = t.inMinutes < 10
+        ? "0" + t.inMinutes.toString()
+        : t.inMinutes.toString();
+    int numSeconds = t.inSeconds - t.inMinutes * 60;
+    String seconds =
+        numSeconds < 10 ? "0" + numSeconds.toString() : numSeconds.toString();
+    String formattedTime = minutes + ":" + seconds;
+    return formattedTime;
+  }
+
+  Stream<TimerModel> stream() async* {
+    yield* Stream.periodic(Duration(seconds: 1), (int a) {
+      String time;
+      if (this._isActive) {
+        _time = _time - Duration(seconds: 1);
+        _radius = _time.inSeconds / _fullTime.inSeconds;
+        if (_time.inSeconds <= 0) {
+          _isActive = false;
+        }
+      }
+      time = returnTime(_time);
+      return TimerModel(
+        time: time,
+        percent: _radius,
+      );
+    });
+  }
 }
